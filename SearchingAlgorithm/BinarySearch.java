587557988a021e310b8b3a3e1a9a3c5e88f00927
[haileymoon] <[moongirl1148@naver.com]>
2021-11-01 19:35:28 +0900

    BinarySearch added

--- SearchingAlgorithm/BinarySearch.java
+++ SearchingAlgorithm/BinarySearch.java
@@ -1,45 +1,42 @@
 package SearchingAlgorithm;
 
 public class BinarySearch {
-    static int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,20,30,50,100};
+//used on SORTED array or list
+//1 2 3 4 5 6 7 8 9 
+    static int[] arr = {1,2,5,6,7,10,20,30,50};
     public static void main(String[] args) {
-        System.out.println(recursiveBinarySearch(0, arr.length-1, 50));
-        System.out.println(normalBinarySearch(0, arr.length-1, 100));
-
+        System.out.println(recursiveBinarySearch(0, arr.length-1, 20));
+        System.out.println(normalBinarySearch(0, arr.length-1, 30));
     }
-    //used SORTED list or array
-    public static int recursiveBinarySearch(int low, int high, int value){
+    public static int recursiveBinarySearch(int start, int end, int value){
         int mid;
-        while (low <= high){
-            mid = (low +high) /2;
-            
+        if(start<=end){
+            mid = (start + end) / 2;
             if(value == arr[mid]){
                 return mid;
             }
-            else if (value < arr[mid]){
-                //from arr[0] to arr[mid-1]
-                return recursiveBinarySearch(low, mid-1, value);
+            else if(value < arr[mid]){
+                return recursiveBinarySearch(start, mid-1, value);
             }
             else{
-                //from arr[mid+1] to arr[high]
-                return recursiveBinarySearch(mid+1, high, value);
+                return recursiveBinarySearch(mid+1, end, value);
             }
         }
         return -1;
-    } 
-    public static int normalBinarySearch(int low, int high, int value){
-        int mid;
+    }
 
-        while(low<=high){
-            mid = (low + high) / 2;
+    public static int normalBinarySearch(int start, int end, int value){
+        int mid;
+        while(start<=end){
+            mid = (start + end) / 2;
             
             if(value == arr[mid]){
                 return mid;
             }
             else if(value < arr[mid]){
-                high = mid - 1;
+                end = mid - 1;
             }else{
-                low = mid + 1;
+                start = mid + 1;
             }
         }
         return -1;
